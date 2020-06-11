ad72fcd8b6cc294bd0dc4421cc85dedd3a393f31
RohanSanjayPawar <rohan.sanjaypawar@accoliteindia.com>
2020-06-11 16:14:25 +0530

    A decentralized system for adding posts and tipping them

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,121 @@
 import React, { Component } from 'react';
-import logo from '../logo.png';
+import Web3 from 'web3';
+import Identicon from 'identicon.js';
 import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json'
+import Navbar from './Navbar'
+import Main from './Main'
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } 
+    else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } 
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    // Load account
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts);
+    this.setState({ account: accounts[0] })
+    // Network ID
+    const networkId = await web3.eth.net.getId();
+    const networdData = SocialNetwork.networks[networkId];
+
+    if(networdData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networdData.address);
+      this.setState({ socialNetwork })
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({ postCount })
+      // Load Posts
+      for(var i = 1 ; i <= postCount ; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post]
+        })
+      }
+      // Sort posts, show highest tipped posts first
+      this.setState({
+        posts: this.state.posts.sort((a, b) => b.tipAmount - a.tipAmount )
+      })
+      this.setState({ loading: false })
+    } else {
+      window.alert('SocialNetwork contract not deployed to detected network')
+    }
+  }
+
+  createPost(content) {
+    this.setState({ loading: true })
+    this.state.socialNetwork.methods.createPost(content).send({ from: this.state.account })
+    .on('error', function(error){ })
+    .on('transactionHash', function(transactionHash){ })
+    .on('receipt', function(receipt){
+      this.setState({ loading: false });
+      console.log(this.state.loading);
+    })
+    .on('confirmation', function(confirmationNumber, receipt){  })
+    .then(function(newContractInstance){
+      this.setState({ loading: false });
+      console.log(this.state.loading);
+    });
+  }
+
+  tipPost(id, tipAmount) {
+    this.setState({ loading: true });
+    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount })
+    .on('error', function(error){ })
+    .on('transactionHash', function(transactionHash){ })
+    .on('receipt', function(receipt){
+      this.setState({ loading: false });
+      console.log(this.state.loading);
+    })
+    .on('confirmation', function(confirmationNumber, receipt){  })
+    .then(function(newContractInstance){
+      this.setState({ loading: false });
+      console.log(this.state.loading);
+    });
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: [],
+      loading: true
+    }
+
+    this.createPost = this.createPost.bind(this);
+    this.tipPost = this.tipPost.bind(this);
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
+        <Navbar account={this.state.account}></Navbar>
+        { this.state.loading
+          ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
+          : <Main 
+              posts={this.state.posts}
+              createPost={this.createPost}
+              tipPost = {this.tipPost}
+            />
+        }
       </div>
     );
   }
